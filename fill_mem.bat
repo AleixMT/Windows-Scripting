47275e7c31088d5d9d87197eb88e9dfe79cd8803
Aleix Mariné Tena <aleix.marine@estudiants.urv.cat>
2022-09-20 13:22:23 +0200

    Scripts to load / kill resident services that I usually use

---
+++ fill_mem.bat
@@ -0,0 +1,8 @@
+start "" "C:\Program Files\qBittorrent\qbittorrent.exe"
+start "" "C:\Program Files (x86)\Dropbox\Client\Dropbox.exe"
+start MEGAsync.exe
+start "" "C:\Program Files\Google\Drive File Stream\61.0.3.0\GoogleDriveFS.exe"
+start "" "C:\Program Files (x86)\AnyDesk\AnyDesk.exe"
+start "" "C:\Users\Aleix\AppData\Local\Microsoft\OneDrive\OneDrive.exe"
+start C:\Users\Aleix\AppData\Local\Discord\Update.exe --processStart Discord.exe
+start "" "C:\Program Files (x86)\Steam\steam.exe"
